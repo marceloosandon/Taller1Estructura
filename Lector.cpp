dfe73d5415a9176ea112a5842ccf488a0dc01f15
Millagi <marcelo.osandon@alumnos.ucn.cl>
2026-09-14 16:41:15 -0300

    con mayuscula

---
+++ Lector.cpp
@@ -0,0 +1,21 @@
+#include "Paciente.hpp"
+#include "Nodo.hpp"
+
+class Lector {
+private:
+    Nodo* primerNodo;
+public:
+    Lector(Nodo* inicio = nullptr) {
+        this->primerNodo = inicio;
+    }
+    Nodo* LeerDesdeElComienzo() {
+        return this->primerNodo;
+    }
+    void mostrarTodosLosPacientes() {
+        Nodo* actual = LeerDesdeElComienzo();
+        while (actual != nullptr) {
+            Paciente p = actual->verPaciente();
+            actual = actual->ObtenerPacienteSiguiente();
+        }
+    }
+};
